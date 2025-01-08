3849a472f7afc255686b31e7414bc8f5eabbc957
simone10522 <simone10522@gmail.com>
2025-01-08 21:51:58 +0100

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -86,7 +86,7 @@ import React, { useState } from 'react';
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
               >
                 <option value="">Select an image</option>
-                <option value="https://github.com/simone10522/LBDBPP/blob/main/icons/profile1.png?raw=true">Profile 1</option>
+                <option value="/icons/profile1.png">Profile 1</option>
                 <option value="https://github.com/simone10522/LBDBPP/blob/main/icons/profile2.png?raw=true">Profile 2</option>
                 <option value="https://github.com/simone10522/LBDBPP/blob/main/icons/profile3.png?raw=true">Profile 3</option>
                 <option value="https://github.com/simone10522/LBDBPP/blob/main/icons/profile4.png?raw=true">Profile 4</option>
